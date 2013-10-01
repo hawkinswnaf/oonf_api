b98c68e9d1764220beaa83f6cdf6ee48dfbf2db6
Will Hawkins <hawkinsw@opentechinstitute.org>
2013-10-01 15:33:34 -0400

    Fix generate_builddata.cmake for version check.

--- cmake/generate_builddata.cmake
+++ cmake/generate_builddata.cmake
@@ -9,11 +9,12 @@ SET(OONF_LIB_GIT "cannot read git repository")
 IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND")
 	# get git description WITH dirty flag
 	execute_process(COMMAND git describe --always --long --tags --dirty --match "v[0-9]*"
-		OUTPUT_VARIABLE OONF_LIB_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
+		OUTPUT_VARIABLE OONF_LIB_GIT_TAG OUTPUT_STRIP_TRAILING_WHITESPACE)
+	STRING(REGEX REPLACE "^v(.*)-[^-]*-[^-]*$" "\\1" OONF_LIB_GIT ${OONF_LIB_GIT_TAG})
 ENDIF()
 
 # compare with version number
-STRING(REGEX MATCH "v${OONF_VERSION}-" FOUND_VERSION ${OONF_LIB_GIT})
+STRING(REGEX MATCH "${OONF_VERSION}" FOUND_VERSION ${OONF_LIB_GIT})
 IF (NOT FOUND_VERSION)
 	message (FATAL_ERROR "Library version '${OONF_VERSION}'"
 		" is not present in git description '${OONF_LIB_GIT}'."
